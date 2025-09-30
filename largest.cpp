817aed47b81b1fe960f7d3eeb71d3da58afab42e
piyushwattamwar <piyush31wattamwar@gmail.com>
2025-09-30 19:02:05 +0530

    First code

---
+++ largest.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maximum_element(int arr[],int n){
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++){
+        maxi=max(maxi,arr[i]);
+    }
+    return maxi;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<maximum_element(arr,n);
+    return 0;
+}
